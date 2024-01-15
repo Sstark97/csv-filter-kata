68e5eb911dca94cd7fb9b27260b0d088f5941add
Sstark97 <aitorscinfo@gmail.com>
2024-01-15 21:28:49 +0000

    :recycle: Extract method to check if all taxes are filled

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -1,9 +1,13 @@
 export class CsvFilter {
     static execute(bills: string[]): string[] {
         const fields: string[] = bills[1].split(",")
-        if(fields[4] !== "" && fields[5] !== "") {
+        if(this.bothTaxesAreFill(fields[4], fields[5])) {
             return [bills[0]]
         }
         return bills
+    }
+
+    private static bothTaxesAreFill(iva: string, igic: string) {
+        return iva !== "" && igic !== "";
     }
 }
