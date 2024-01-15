5ca30f687df4738e50682ff82038ffe1d7f7b16f
Sstark97 <aitorscinfo@gmail.com>
2024-01-15 21:24:13 +0000

    Test(:white_check_mark:): CsvFilter should remove a bill if IVA and IGIC field are both fill

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -1,5 +1,9 @@
 export class CsvFilter {
-    static execute(bills: string): string {
+    static execute(bills: string[]): string[] {
+        const fields: string[] = bills[1].split(",")
+        if(fields[4] !== "" && fields[5] !== "") {
+            return [bills[0]]
+        }
         return bills
     }
 }
--- src/test/CsvFilter.test.ts
+++ src/test/CsvFilter.test.ts
@@ -2,18 +2,16 @@ import {CsvFilter} from "../core/CsvFilter";
 
 describe("CsvFilter", () => {
     it("should produce the same for a file with one correct bill", () => {
-        const bills: string =
-            "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n" +
-            "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,"
+        const header: string = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        const bill: string = "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,"
 
-        expect(CsvFilter.execute(bills)).toBe(bills)
+        expect(CsvFilter.execute([header, bill])).toStrictEqual([header, bill])
     })
 
     it("should remove a bill if IVA and IGIC field are both fill", () => {
-        const bills: string =
-            "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n" +
-            "1,02/05/2019,1008,810,19,17,ACERLaptop,B76430134,"
+        const header: string = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        const bill: string = "1,02/05/2019,1008,810,19,17,ACERLaptop,B76430134,"
 
-        expect(CsvFilter.execute(bills)).toBe("Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n")
+        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
     })
 })
