e3fbdc7235bdba74fa50b53f4440bf3b8a09f73d
Sstark97 <aitorscinfo@gmail.com>
2024-01-15 21:01:24 +0000

    Test(:rotating_light:): CsvFilter should produce the same for a file with one correct bill

--- src/test/CsvFilter.test.ts
+++ src/test/CsvFilter.test.ts
@@ -1,5 +1,15 @@
+class CsvFilter {
+    static execute(bills: string): string {
+        return ""
+    }
+}
+
 describe("CsvFilter", () => {
-    it("world!", () => {
-        expect(true).toBe(true)
+    it("should produce the same for a file with one correct bill", () => {
+        const bills: string =
+            "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente\n" +
+            "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,"
+
+        expect(CsvFilter.execute(bills)).toBe(bills)
     })
 })
