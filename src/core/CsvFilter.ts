51047c09c1b374a2b4f4f190b6374e60dedf12b4
Sstark97 <aitorscinfo@gmail.com>
2024-01-17 12:23:03 +0000

    Test(:white_check_mark:): CsvFilter should remove a bill if any taxes are non decimal

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -2,6 +2,9 @@ export class CsvFilter {
     static execute(bills: string[]): string[] {
         const fields: string[] = bills[1].split(",")
         const [iva, igic, cif, nif] = [fields[4], fields[5], fields[7], fields[8] ?? ""]
+        if (isNaN(Number(iva)) || isNaN(Number(igic))) {
+            return [bills[0]]
+        }
         if(this.bothTaxesAreFill(iva, igic) || this.bothTaxesAreEmpty(iva, igic) || this.bothIdentifiersAreFill(cif, nif)) {
             return [bills[0]]
         }
--- src/test/CsvFilter.test.ts
+++ src/test/CsvFilter.test.ts
@@ -22,8 +22,8 @@ describe("CsvFilter", () => {
     })
 
     it("should remove a bill if any taxes are non decimal", () => {
-        const billWithIncorrectIva: string = "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134"
-        const billWithIncorrectIgic: string = "1,02/05/2019,1008,810,,17,ACERLaptop,B76430134"
+        const billWithIncorrectIva: string = "1,02/05/2019,1008,810,ITAX,,ACERLaptop,B76430134"
+        const billWithIncorrectIgic: string = "1,02/05/2019,1008,810,,ITAX,ACERLaptop,B76430134"
 
         expect(CsvFilter.execute([header, billWithIncorrectIva])).toStrictEqual([header])
         expect(CsvFilter.execute([header, billWithIncorrectIgic])).toStrictEqual([header])
