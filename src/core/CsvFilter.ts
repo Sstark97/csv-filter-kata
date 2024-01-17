f61c8f81220d63e8b32dcb0a3696d8bea16566a3
Sstark97 <aitorscinfo@gmail.com>
2024-01-17 12:25:33 +0000

    :recycle: Extract all taxes validation to another method

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -2,13 +2,15 @@ export class CsvFilter {
     static execute(bills: string[]): string[] {
         const fields: string[] = bills[1].split(",")
         const [iva, igic, cif, nif] = [fields[4], fields[5], fields[7], fields[8] ?? ""]
-        if (isNaN(Number(iva)) || isNaN(Number(igic))) {
-            return [bills[0]]
-        }
-        if(this.bothTaxesAreFill(iva, igic) || this.bothTaxesAreEmpty(iva, igic) || this.bothIdentifiersAreFill(cif, nif)) {
+
+        if(this.taxFieldsAreExclusive(iva, igic) || this.bothIdentifiersAreFill(cif, nif)) {
             return [bills[0]]
         }
         return bills
+    }
+
+    private static taxFieldsAreExclusive(iva: string, igic: string) {
+        return this.bothTaxesAreFill(iva, igic) || this.bothTaxesAreEmpty(iva, igic) || isNaN(Number(iva)) || isNaN(Number(igic));
     }
 
     private static bothTaxesAreEmpty(iva: string, igic: string) {
