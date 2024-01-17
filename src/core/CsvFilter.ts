267a2a946e5996308d9e3c3c8c47f6b8cdacda5d
Sstark97 <aitorscinfo@gmail.com>
2024-01-17 12:12:52 +0000

    Test(:rotating_light:): CsvFilter should remove a bill if all taxes field are empty as one is required

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -1,7 +1,7 @@
 export class CsvFilter {
     static execute(bills: string[]): string[] {
         const fields: string[] = bills[1].split(",")
-        const [iva, igic, cif, nif] = [fields[4], fields[5], fields[7], fields[8]]
+        const [iva, igic, cif, nif] = [fields[4], fields[5], fields[7], fields[8] ?? ""]
         if(this.bothTaxesAreFill(iva, igic) || this.bothIdentifiersAreFill(cif, nif)) {
             return [bills[0]]
         }
--- src/test/CsvFilter.test.ts
+++ src/test/CsvFilter.test.ts
@@ -21,4 +21,11 @@ describe("CsvFilter", () => {
 
         expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
     })
+
+    it("should remove a bill if all taxes field are empty as one is required", () => {
+        const header: string = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
+        const bill: string = "1,02/05/2019,1008,810,,,ACERLaptop,B76430134"
+
+        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
+    })
 })
