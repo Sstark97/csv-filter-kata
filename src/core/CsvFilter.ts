17c2500d54acb31cefa0310923492a323a15186a
Sstark97 <aitorscinfo@gmail.com>
2024-01-17 12:15:35 +0000

    :recycle: Extract all identifiers are empty to a method

--- src/core/CsvFilter.ts
+++ src/core/CsvFilter.ts
@@ -2,10 +2,14 @@ export class CsvFilter {
     static execute(bills: string[]): string[] {
         const fields: string[] = bills[1].split(",")
         const [iva, igic, cif, nif] = [fields[4], fields[5], fields[7], fields[8] ?? ""]
-        if(this.bothTaxesAreFill(iva, igic) || this.bothIdentifiersAreFill(cif, nif) || iva === "" && igic === "") {
+        if(this.bothTaxesAreFill(iva, igic) || this.bothTaxesAreEmpty(iva, igic) || this.bothIdentifiersAreFill(cif, nif)) {
             return [bills[0]]
         }
         return bills
+    }
+
+    private static bothTaxesAreEmpty(iva: string, igic: string) {
+        return iva === "" && igic === "";
     }
 
     private static bothIdentifiersAreFill(cif: string, nif: string) {
