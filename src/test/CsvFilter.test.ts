aca67ddf24a32b831b169051d260a5a1a382339d
Sstark97 <aitorscinfo@gmail.com>
2024-09-24 08:15:34 +0100

    :recycle: Extract methods to construct an invoice

--- src/test/CsvFilter.test.ts
+++ src/test/CsvFilter.test.ts
@@ -3,42 +3,48 @@ import {CsvFilter} from "../core/CsvFilter";
 describe("CsvFilter", () => {
     const header: string = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente"
 
-    it("should produce the same for a file with one correct bill", () => {
-        const bill: string = "1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,"
+    describe("with taxes", () => {
+        it("should remove a invoice if IVA and IGIC field are both fill", () => {
+            expect(CsvFilter.execute([header, anInvoiceWithOneLine("19", "17")])).toStrictEqual([header])
+        })
 
-        expect(CsvFilter.execute([header, bill])).toStrictEqual([header, bill])
+        it("should remove a invoice if all taxes field are empty as one is required", () => {
+            expect(CsvFilter.execute([header, anInvoiceWithOneLine("", "")])).toStrictEqual([header])
+        })
+
+        it("should remove a invoice if any taxes are non decimal", () => {
+            const invoiceWithIncorrectIva: string = anInvoiceWithOneLine("ITAX", "")
+            const invoiceWithIncorrectIgic: string = anInvoiceWithOneLine("", "ITAX")
+
+            expect(CsvFilter.execute([header, invoiceWithIncorrectIva])).toStrictEqual([header])
+            expect(CsvFilter.execute([header, invoiceWithIncorrectIgic])).toStrictEqual([header])
+        })
     })
 
-    it("should remove a bill if IVA and IGIC field are both fill", () => {
-        const bill: string = "1,02/05/2019,1008,810,19,17,ACERLaptop,B76430134,"
-
-        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
+    it("should produce the same for a file with one correct invoice", () => {
+        const invoice = anInvoiceWithOneLine("21", "")
+        expect(CsvFilter.execute([header, invoice])).toStrictEqual([header, invoice])
     })
 
-    it("should remove a bill if all taxes field are empty as one is required", () => {
-        const bill: string = "1,02/05/2019,1008,810,,,ACERLaptop,B76430134"
+    it("should remove a invoice if CIF and NIF field are both fill", () => {
+        const invoice: string = anInvoiceWithOneLine("", "17")
 
-        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
+        expect(CsvFilter.execute([header, `${invoice},34214567Z`])).toStrictEqual([header])
     })
 
-    it("should remove a bill if any taxes are non decimal", () => {
-        const billWithIncorrectIva: string = "1,02/05/2019,1008,810,ITAX,,ACERLaptop,B76430134"
-        const billWithIncorrectIgic: string = "1,02/05/2019,1008,810,,ITAX,ACERLaptop,B76430134"
+    it("should remove a invoice if net amount is not calculated correctly", () => {
+        const invoice: string = anInvoiceWithOneLine("", "")
 
-        expect(CsvFilter.execute([header, billWithIncorrectIva])).toStrictEqual([header])
-        expect(CsvFilter.execute([header, billWithIncorrectIgic])).toStrictEqual([header])
+        expect(CsvFilter.execute([header, invoice])).toStrictEqual([header])
     })
 
-
-    it("should remove a bill if CIF and NIF field are both fill", () => {
-        const bill: string = "1,02/05/2019,1008,810,,17,ACERLaptop,B76430134,34214567Z"
-
-        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
-    })
-
-    it("should remove a bill if net amount is not calculated correctly", () => {
-        const bill: string = "1,02/05/2019,1000,820,,17,ACERLaptop,B76430134"
-
-        expect(CsvFilter.execute([header, bill])).toStrictEqual([header])
-    })
+    const anInvoiceWithOneLine = (iva: string, igic: string) => {
+        const invoiceId = "1";
+        const invoiceDate = "02/05/2019";
+        const gross = "1008";
+        const net = "796.32";
+        const concept = "ACERLaptop";
+        const clientId = "B76430134";
+        return [invoiceId,invoiceDate,gross,net,iva,igic,concept,clientId].join(",");
+    };
 })
